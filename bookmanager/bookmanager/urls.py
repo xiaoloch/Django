36682c20a1d5fed6981d75c982573a6126d63f55
chenxia <chenxlong@126.com>
2022-04-26 17:44:16 +0800

    视图和url

--- bookmanager/bookmanager/urls.py
+++ bookmanager/bookmanager/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
+# from book.views import index
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # 添加一个路径
+    # path('路由','视图函数名')
+    path('blog/',include('book.urls')),
 ]
